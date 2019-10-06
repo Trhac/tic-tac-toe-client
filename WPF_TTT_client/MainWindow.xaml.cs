6a13ff1c9f3027638ee9e41d8d415b5684e22fa7
Trhac <trhac@live.com>
2019-10-06 15:56:38 +0200

    Pridana prvotna GET/PUSH funkcionalita

--- WPF_TTT_client/MainWindow.xaml.cs
+++ WPF_TTT_client/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,41 @@ namespace WPF_TTT_client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly HttpClient client = new HttpClient();
+        static String serverURL = "http://145.239.95.192:4000";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            
+            String message = textBoxInput.Text;
+
+            //odoslat na server
+            Uri uriPost = new Uri(serverURL + "/");
+
+            var values = new Dictionary<string, string>
+            {
+            { "sprava", message }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(uriPost, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            MessageBox.Show(responseString);
+            
+        }
+
+        private async void ButtonGet_Click(object sender, RoutedEventArgs e)
+        {
+            var responseString = await client.GetStringAsync(serverURL + "/");
+            MessageBox.Show(responseString);
         }
     }
 }
